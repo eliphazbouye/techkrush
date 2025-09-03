4a8dd29df92946ada3bd59799cd980c1babaa49c
Eliphaz Bouye <eliphazbouye@gmail.com>
2025-09-03 14:39:10 +0000

    feat: add fortify for auth

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    /**
+     * Get the posts that belong to the tag.
+     *
+     * @return BelongsToMany
+     */
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
